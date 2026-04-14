53ccc4d953b1f0d23d871782d31411318461c99a
Vincenzo Ferrucci <119014298+Vinsss031@users.noreply.github.com>
2026-04-14 11:20:57 +0200

    [WIP] main

--- main.m
+++ main.m
@@ -5,8 +5,13 @@ clear; close all; clc
 addpath(genpath("./src"))
 plotFlag = 1;
 
+if ~exist(fullfile('.', 'data'), 'dir') || ~exist(fullfile('.', 'data', 'propellant.mat'), 'file')
+    data = savePropellantData();
+else
+    data = load(fullfile('.', 'data', 'propellant.mat'));
+end
 
-%% Input Data
+%% Configuraiton Data
 
 % Generic constants
 constants = Constants();
@@ -21,48 +26,28 @@ pe = constants.pAmb;                                                        % [P
 % Area ratios (wrt At) from which to which there is active cooling
 coolARatios = [2, 2];                                            
 
-% Empirical data
-% Pressure [bar]
-pressureData = [10.1; 9.7; 10.3; 30.2; 31.0; 29.8; 50.2; 51.0; ...
-            50.3; 70.2; 69.0; 69.8; 91.2; 89.1; 89.0];
-
-% Burning rate [mm/s]
-burningRateData = [4.0; 3.8; 4.1; 5.6; 6.0; 5.7; 7.0; 7.2; ...
-                7.1; 8.4; 8.3; 8.6; 8.8; 9.0; 9.2];
-
-% from NASA CEA output- composition: 80% AP - 20% HTPB
-gamma = 1.2386*1.00032;                                                     % Specific heat ratio (gammas * (dLV/dLP)t) [-]
-Tc = 2342.92;                                                               % Chamber temperature [K]
-rhoAp = 1950;                                                               % Ammonium perchlorate density [kg/m^3]
-rhoHTPB = 913;                                                              % HTPB density [kg/m^3]
-molarMassProp = 22.087;
-rhoP = 1/(0.8/rhoAp +  0.2/rhoHTPB);                                        % Propellant density [kg/m^3]
-
-
-%% 
-
 % Vieille's law
-[a, aSigma, n, nSigma, R2] = Uncertainty(pressureData, burningRateData);
+[a, aSigma, n, nSigma, R2] = uncertaintyVieille(data.ccPressure, data.burnRate);
 
 
 % Conical nozzleDesign
-[nozzle, propPerf] = nozzleDesign(thrust, Tc, pc, pe, gamma, molarMassProp, ...
+[nozzle, propPerf] = nozzleDesign(thrust, data.cea.ccTemperature, pc, pe, data.cea.gamma, data.cea.molarMass, ...
                                     constants, ...
                                     "alpha", 15 * pi/180, ...
                                     "beta", 30 * pi/180);
 
 mPTot = propPerf.mDot*burningTime;                                          % Total propellant mass [kg]
 
-rb = a*(pc*1e-5)^n;                                                         % Burning rate with nominal chamber pressure [mm/s]
+rb = a*pc^n;                                                         % Burning rate with nominal chamber pressure [mm/s]
 web = rb*burningTime;
-Vprop = mPTot/rhoP;
+Vprop = mPTot/data.cea.rhoP;
 
 % BATES motor design (using Richard Nakka formulation)
-dInt = internalDiameter(Vprop, web*1e-3); % m
-[dExt, L0] = grainConfiguration(dInt, web*1e-3); % m
+% dInt = internalDiameter(Vprop, web*1e-3); % m
+[dExt, L0] = grainConfiguration(Vprop, web*1e-3); % m
 
 if plotFlag, plotHollowCylinder(dExt, dInt, L0); end
 
 % Combustion chamber
-% [t, p, rb] = computeBurn(a, n, rhoP*1e-6, propPerf.cstar, dExt, dInt, L0, nozzle.At*1e6);
+% [t, p, rb] = computeBurn(a, n, data.cea.rhoP*1e-6, propPerf.cstar, dExt, dInt, L0, nozzle.At*1e6);
 
